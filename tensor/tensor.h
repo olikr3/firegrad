fcae0be244709a38bc75a7cbcacc388563517ffb
olikr3 <oliver.kremser@gmail.com>
2024-09-04 15:07:32 +0200

    added tensor operations

--- tensor/tensor.h
+++ tensor/tensor.h
@@ -1,9 +1,10 @@
-#pragma once
+#ifndef TENSOR_H
+#define TENSOR_H
 
 #include<vector>
 
 
-class Tensor:
+class Tensor {
 
 private:
     std::vector<float> data;
@@ -27,3 +28,47 @@ public:
         std::multiplies<size_t>());
         data.resize(total_size);
     }
+
+    float& operator()(const std::vector<size_t>& indices) {
+        size_t offset = compute_offset(indices);
+        return data[offset];
+    }
+
+    const float& operator()(const std::vector<size_t>& indices) const {
+        size_t offset = compute_offset(indices);
+        return data[offset];
+    }
+
+    const std::vector<size_t>& getShape() const {
+        return shape;
+    }
+
+    size_t size() const {
+        return total_size;
+    }
+
+    Tensor operator+(const Tensor& other) const {
+        assert(shape == other.shape);
+        Tensor result_Tensor(shape);
+        for (size_t i = 0; i < total_size; i++) {
+            result_Tensor.data[i] = data[i] + other.data[i];
+        }
+        return result_Tensor;
+    }
+
+    Tensor operator-(const Tensor& other) const {
+        assert(shape == other.shape);
+        Tensor result_Tensor(shape);
+        for (size_t i = 0; i < total_size; i++){
+            result_Tensor[i] = data[i] - other.data[i];
+        }
+        return result_Tensor;
+    }
+
+    //not implemented
+    Tensor operator*(const Tensor& other) const {
+        Tensor result_Tensor(shape);
+        return result_Tensor
+    }
+
+}
