7feed04d37c0775591d27efc1d89a9aace751bb3
olikr3 <oliver.kremser@gmail.com>
2024-09-11 11:10:23 +0200

    modified tensor class

--- firegrad/tensor/tensor.py
+++ firegrad/tensor/tensor.py
@@ -11,6 +11,33 @@ class Tensor:
         self._prev = set(children)
         self._ops = ops
 
-    def __add__(self, other):
+    def __add__(self, other, alpha=1):
+        """
+        returns a new tensor: result = self + alpha*other
+        """
         assert isinstance(other, Tensor)
-        result = Tensor(self.data + other.data)
+        result = Tensor(self.data + (alpha*other.data))
+        
+        return result
+    
+    def __sub__(self, other, alpha=1):
+        """
+        returns a new tensor: result = self - alpha*other
+        """
+        assert isinstance(other, tensor)
+        return Tensor(self.data + (-1 * alpha * other.data)
+
+
+    def __mul__(self, other):
+        assert isinstance(other, Tensor)
+        result = Tensor(self.data * other.data)
+        
+        return result
+
+    def __neg__(self, other):
+        result = Tensor(-1 * self.data)
+        return result
+
+    def __str__(self):
+        return f"firegrad.tensor{self.data}, {self.grad}"
+
